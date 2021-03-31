be85bb53a1aa3b6ea8c3866a87763aeecf0077ac
Shorl <milanarif@gmail.com>
2021-03-31 11:59:03 +0200

    Working commit, Jenkins pushes to DockerHub, tests detect fail

--- src/main/java/stringop/StringOperator.java
+++ src/main/java/stringop/StringOperator.java
@@ -5,6 +5,7 @@ import com.google.common.base.CharMatcher;
 public class StringOperator {
    public static void main(String[] args) {
       returnHello();
+      System.out.println("HEJ");
    }
 
    public static String returnHello(){
